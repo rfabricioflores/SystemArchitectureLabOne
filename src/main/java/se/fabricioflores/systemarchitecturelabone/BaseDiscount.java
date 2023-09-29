4efc08f35b8693070fe4628bd3e14812a73f202c
Fabricio Flores <me@fabricioflores.se>
2023-09-29 13:11:26 +0200

    init commit

--- src/main/java/se/fabricioflores/systemarchitecturelabone/BaseDiscount.java
+++ src/main/java/se/fabricioflores/systemarchitecturelabone/BaseDiscount.java
@@ -10,19 +10,16 @@ public abstract class BaseDiscount implements Discount {
     public abstract boolean isApplicable(Product product);
     public abstract double calculateDiscount(Product product);
 
-    // Applicerar rabatten på produkten och returnerar rabattbeloppet
     public double apply(Product product) {
         double discountAmount = 0.0;
 
         if(isApplicable(product)) {
             discountAmount = calculateDiscount(product);
-            product.setPrice(product.getPrice() - discountAmount);
         }
 
         return discountAmount + nextDiscount.apply(product);
     }
 
-    // Returnerar beskrivning av rabatten som textsträng
     public String getDescription(Product product) {
         return nextDiscount.getDescription(product);
     }
--- src/main/java/se/fabricioflores/systemarchitecturelabone/Main.java
+++ src/main/java/se/fabricioflores/systemarchitecturelabone/Main.java
@@ -11,16 +11,10 @@ public class Main {
 //        System.out.println("Discount amount: " + discount.apply(p3));
 //        System.out.println(discount.getDescription(p3));
 //
-//        System.out.println("Final price: "+ p3.getPrice());
 
 
-        //  Get negative price cuz I am applying discount to the price of one product when the product quantity is 7.
-        //  I could fix this by overriding the apply method for QuantityDiscount but i still don't know if should change the price of the product or not
-        // ???
-        Discount discount = new QuantityDiscount((new RootDiscount()));
-        System.out.println("Discount amount: " + discount.apply(p2));
+        Discount discount = new FridayDiscount(new QuantityDiscount((new RootDiscount())));
+        System.out.println("Discount amount: " + discount.apply(p2) + "kr");
         System.out.println(discount.getDescription(p2));
-
-        System.out.println("Final price: "+ p2.getPrice());
     }
 }
