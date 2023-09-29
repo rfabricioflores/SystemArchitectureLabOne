7f54f0f92dcd0ee78a740324d2af269df3f6d19d
Fabricio Flores <me@fabricioflores.se>
2023-09-29 18:25:27 +0200

    init commit

--- src/main/java/se/fabricioflores/systemarchitecturelabone/Main.java
+++ src/main/java/se/fabricioflores/systemarchitecturelabone/Main.java
@@ -6,15 +6,21 @@ public class Main {
         Product p2  = new Product("Camera", 15, 7);
         Product p3 = new Product("Milk", 3, 2);
 
+        Discount discount = new MilkDiscount(new QuantityDiscount(new FridayDiscount((new RootDiscount()))));
 
-//       Discount discount = new MilkDiscount(new FridayDiscount(new QuantityDiscount((new RootDiscount()))));
-//        System.out.println("Discount amount: " + discount.apply(p3));
-//        System.out.println(discount.getDescription(p3));
-//
+        System.out.println("Ball");
+        System.out.println("-----");
+        System.out.println("Discount amount: " + discount.apply(p1) + "kr");
+        System.out.println(discount.getDescription(p1));
 
-
-        Discount discount = new FridayDiscount(new QuantityDiscount((new RootDiscount())));
+        System.out.println("\nCamera");
+        System.out.println("--------");
         System.out.println("Discount amount: " + discount.apply(p2) + "kr");
         System.out.println(discount.getDescription(p2));
+
+        System.out.println("\nMilk");
+        System.out.println("--------");
+        System.out.println("Discount amount: " + discount.apply(p3) + "kr");
+        System.out.println(discount.getDescription(p3));
     }
 }
